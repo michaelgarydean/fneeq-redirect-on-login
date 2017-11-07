eaa852fead66f018794657984251a7c220080d15
Michael Dean <mike@koumbit.org>
2017-11-07 11:29:48 -0500

    initial commit

---
+++ fneeq-redirect-on-login.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     FNEEQ Redirect User on Login
+ * Plugin URI:      https://github.com/mykedean/fneeq-redirect-on-login
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          Michael Dean
+ * Author URI:      https://github.com/mykedean
+ * Text Domain:     fneeq-redirect-on-login
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Fneeq_Redirect_On_Login
+ */
